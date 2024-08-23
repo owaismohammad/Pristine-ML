0235825b8d437efbef36b1f1de7c9e0dfef4acb8
owaismohammad <141389451+owaismohammad@users.noreply.github.com>
2024-08-23 22:41:57 +0530

    Update model.py

--- models/model.py
+++ models/model.py
@@ -5,9 +5,9 @@ class Model():
     def __init__(self):
         self.model=tf.keras.models.load_model(r'water_pred_02.h5')
 
-    def image_to_feature_vector(self,image_path, size=(128, 128)):
+    def image_to_feature_vector(self,image, size=(128, 128)):
       # Load the image using OpenCV
-      image = cv2.imread(image_path)
+      image = cv2.imread(image)
       # Resize the image to the specified size
       image = cv2.resize(image, size)
 
