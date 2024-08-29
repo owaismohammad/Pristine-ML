c65aab30a610ffd8d869ff6144709b051a525110
Mohammad <owaismohammad2515@gmail.com>
2024-08-29 16:35:28 +0530

    added improved model

--- models/model.py
+++ models/model.py
@@ -3,7 +3,7 @@ import cv2
 
 class Model():
     def __init__(self):
-        self.model=tf.keras.models.load_model(r'water_pred_02.h5')
+        self.model=tf.keras.models.load_model(r'water_pred_improved_added_cmnts.h5')
 
     def image_to_feature_vector(self,image_get, size=(128, 128)):
       # Load the image using OpenCV
