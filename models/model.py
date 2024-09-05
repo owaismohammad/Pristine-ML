59ea4247cf9d195118ee7b945ff35ab1806ab247
Mohammad Owais <141389451+owaismohammad@users.noreply.github.com>
2024-09-06 01:28:15 +0530

    Update model.py

--- models/model.py
+++ models/model.py
@@ -5,13 +5,13 @@ class Model():
     def __init__(self):
         self.model=tf.keras.models.load_model(r'water_pred_improved_added_cmnts.h5')
 
-    def image_to_feature_vector(self,image_get, size=(128, 128)):
+    def image_to_feature_vector(self,image_get, size=(64, 64)):
       # Load the image using OpenCV
       # image = cv2.imread(image)
       # Resize the image to the specified size
       image = cv2.resize(image_get, size)
         
-      self.image=image.reshape((1,128,128,3))
+      self.image=image.reshape((1,64,64,3))
       self.image=self.image/255.0
 
     def image_prediction(self,image):
